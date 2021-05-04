7585c4625902d66fb591c1b6334d3eb23bba99f3
Unagi <62870460+UnagiAdmin@users.noreply.github.com>
2021-05-04 08:28:06 +0800

    Update useGetDocumentTitlePrice.ts

--- src/hooks/useGetDocumentTitlePrice.ts
+++ src/hooks/useGetDocumentTitlePrice.ts
@@ -16,7 +16,7 @@ const useGetDocumentTitlePrice = () => {
         })}`
 
   useEffect(() => {
-    document.title = `PancakeSwap${cakePriceUsdString}`
+    document.title = `UnagiSwap Finance`
   }, [cakePriceUsdString])
 }
 export default useGetDocumentTitlePrice
